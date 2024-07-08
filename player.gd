b74a6a43c00fab9cb05fbf39ca00f45724734760
tajsDev <tomasjauregui14@gmail.com>
2024-07-08 15:29:30 -0700

    added more stuff to player and world, intial stage

--- player.gd
+++ player.gd
@@ -10,12 +10,11 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
+	#if not is_on_floor():
+		#velocity.y -= gravity * delta
 	# Handle jump.
 	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
-
+		#velocity.y = JUMP_VELOCI
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
